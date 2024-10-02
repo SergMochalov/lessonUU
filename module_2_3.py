f7b7f860e46e58ee9e5927e66d7e549191cef38a
SergMochalov <Serginho19860207@yandex.ru>
2024-10-02 18:20:06 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+x = 0
+new_my_list = []
+end = len(my_list)
+while x in range(end):
+    if my_list[x] >= 0:
+        if my_list[x] > 0:
+            print(my_list[x])
+        x += 1
+    else:
+        break

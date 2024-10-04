6bd4e3480e5e71014f6b541cf936e5141d2a39d5
SergMochalov <Serginho19860207@yandex.ru>
2024-10-04 21:01:50 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,13 +1,15 @@
 def get_matrix(n, m, value):
     matrix = []
-    for x in range(n):
-        matrix.append([])
-        for y in range(m):
-            matrix[x].append(value)
-    print(matrix)
+    if n>0 and m>0:
+        for x in range(n):
+            matrix.append([])
+            for y in range(m):
+                matrix[x].append(value)
+    return matrix
 
-
-
-get_matrix(2, 2, 10)
-get_matrix(3, 5, 42)
-get_matrix(4, 2, 13)
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)

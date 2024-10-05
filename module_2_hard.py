8f89f4c7841a3e829f22a9eeb4319b7b3bc2a6d1
SergMochalov <Serginho19860207@yandex.ru>
2024-10-05 17:08:39 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,16 +2,16 @@ import random
 n=random.randint(3,20)
 print(n)
 result = []
+slow = []
 for x in range(1,20):
     for y in range(1, 20):
-        if x!=y:
-            m=x+y
+        if x != y and x > y:
+            m = x + y
             if x < n and y < n:
                 if (m % n) == 0 or (n % m) == 0:
-
                     result.append(x)
                     result.append(y)
-
+                    slow.append(x)
                 if (m % n) != 0 or (n % m) != 0:
                     continue
             else:
@@ -19,3 +19,4 @@ for x in range(1,20):
         else:
             continue
 print(result)
+print(slow)
